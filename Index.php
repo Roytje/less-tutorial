0b5afaccb14101fce2728c70b162708cae5f480f
Roy <roykuijper1997@hotmail.com>
2016-05-11 14:15:27 +0200

    First commit

--- Index.php
+++ Index.php
@@ -1,4 +1,16 @@
-<?php
-    echo "Hallo less wereld";
+<!DOCTYPE html>
+<html>
+    <head>
+       <title>LESS</title> 
+       <link rel="stylesheet/less"  type="text/css" href="./style.less"> 
+       <script src="./less.js/dist/less.js"></script> 
+    </head>
+    <body>
+        <h3>LESS tutorial</h3>
 
-?>
+        <ul>
+            <li><a href="intro_less.php">Introductie en setup Less</a></li>
+            <li><a href="table_less.php">Tabel opmaak met Less</a></li>            
+        </ul>
+    </body>
+</html>
