64774ffe5c4cebf96f93ea56d868eace9865d8e4
Roy <roykuijper1997@hotmail.com>
2016-05-11 15:13:19 +0200

    2e commit

--- Index.php
+++ Index.php
@@ -10,7 +10,9 @@
 
         <ul>
             <li><a href="intro_less.php">Introductie en setup Less</a></li>
-            <li><a href="table_less.php">Tabel opmaak met Less</a></li>            
+            <li><a href="table_less.php">Tabel opmaak met Less</a></li>       
+            <li><a href="lessphp_example.php">Less waarden veranderen met php (serverside)</a></li>  
+            <li><a href="lessphp_example_compilefile.php">Less waarden veranderen met php (serverside)andere</a></li>                            
         </ul>
     </body>
 </html>
